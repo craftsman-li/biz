c406ca9422ee87fdc94bb85ec666bfe494dc8c63
tianwei <isr910523@gmail.com>
2018-09-01 20:38:55 +0800

    add login check

--- bizconfig/config.go
+++ bizconfig/config.go
@@ -4,7 +4,7 @@ package bizconfig
 type DbConfig struct {
 	Dialect string `yaml:"dialect"`
 	Address string `yaml:"address"`
-	Prefix string `yaml:"prefix"`
+	Prefix  string `yaml:"prefix"`
 }
 
 // session biz config
@@ -17,4 +17,7 @@ type LogInfo struct {
 
 // web biz config
 type WebConfig struct {
+	// 是否校验允不允许登录
+	// 如果为true, 则在账号密码验证通过后，检验是否有此路径登录的权限
+	LoginCheckEnable bool
 }
